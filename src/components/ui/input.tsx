6206efe0868dec77544feb123c32a6c7915c3348
flavioespinoza <flavio.espinoza@gmail.com>
2025-03-25 23:37:08 -0600

    fix: replace empty interface with type in Input component to satisfy ESLint rule

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,15 +1,22 @@
 import * as React from 'react'
+import { cn } from '@/lib/utils'
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
+type InputProps = React.InputHTMLAttributes<HTMLInputElement>
 
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, ...props }, ref) => {
-	return (
-		<input
-			ref={ref}
-			className={`border-input bg-background ring-offset-background placeholder:text-muted-foreground focus:ring-ring flex h-10 w-full rounded-md border px-3 py-2 text-sm focus:ring-2 focus:ring-offset-2 focus:outline-none disabled:cursor-not-allowed disabled:opacity-50 ${className}`}
-			{...props}
-		/>
-	)
+const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+  return (
+    <input
+      type={type}
+      className={cn(
+        'flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background',
+        'placeholder:text-muted-foreground focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+        'disabled:cursor-not-allowed disabled:opacity-50',
+        className
+      )}
+      ref={ref}
+      {...props}
+    />
+  )
 })
 Input.displayName = 'Input'
 
